746cbac0ff61c49079ea0556f813a91e4673d61f
truongvi <truongvi1511@gmail.com>
2025-05-06 23:12:07 +0700

    done login

---
+++ InnerChildApi/Contract/Dtos/Responses/PreLoginResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract.Dtos.Responses
+{
+    public class PreLoginResponse
+    {
+        public string UserId { get; set; }
+        public string ProfileId { get; set; }
+        public string ProfileStatus { get; set; }
+        public string ProfileToken { get; set; }
+    }
+}
